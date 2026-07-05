05c981430c21d42eeddf0f80087c8690cfb61618
twalla26 <ssm4748@naver.com>
2026-07-05 10:12:24 +0900

    commit for test

---
+++ src/week1/귤고르기/twalla/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
